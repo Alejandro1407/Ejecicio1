deab1f78f80a41abdd925127a41a86a23662471c
Alejandro1407 <alejandroalejo714@gmail.com>
2020-11-18 10:09:05 -0600

    commit

---
+++ Ejercicio1.js
@@ -0,0 +1,19 @@
+function numerosPrimos(){
+    let numeros = [];
+    for(let i = 1;  numeros.length < 10;i++){
+        let divisores = []
+        for(let j = i;j > 0;j--){
+            if( i % j == 0){
+                divisores.push(j);
+            } 
+        }
+        if(divisores.length <= 2){
+            numeros.push(i);
+        }
+    }
+    let f = document.getElementById("result");
+    f.innerHTML(numeros);
+}
+
+
+//numeros.length <= 1
